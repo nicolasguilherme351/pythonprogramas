660b5bddec56afe6dd9357dde00928f6c155dcad
NICOLAS GUILHERME PIRES DOMINGOS <025963@fatecararas.com>
2025-02-20 12:28:01 -0300

    Starting learning of OOP in python

---
+++ learningOOP-Python/class_And_Objects/program_1.py
@@ -0,0 +1,6 @@
+name = "Danny" 
+age = 29
+
+print(type(name))
+print(name.upper()) # A class String that have a function
+print(type(age))
---
+++ learningOOP-Python/class_And_Objects/program_2.py
@@ -0,0 +1,9 @@
+class Dog: 
+    def bark(self): # Defines method for the class Dog
+        print("Whoof Whoof")
+
+dog = Dog()
+dog.bark()
+
+dog2 = Dog()
+dog2.bark()
