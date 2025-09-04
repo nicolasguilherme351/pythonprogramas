9ddcc1865596dba6017f8ec5e8b2159a269370a8
Nicolas <nicolasrenovado12@gmail.com>
2025-09-03 21:22:16 -0300

    Adicionando programa que defini um comprimento de um arco

---
+++ definirUmComprimentoDeUmArco.py
@@ -0,0 +1,5 @@
+medidaAngular = int(input("Qual é a medida angular? "))
+raio = int(input("Qual é o raio? "))
+comprimentoArco = medidaAngular * raio
+
+print("O comprimento do arco é de " + comprimentoArco)
